98f30023fa8eeeff98aaaea6a3567e5c2bb9bfbf
Franco Ortega <francopdx@gmail.com>
2021-11-02 09:08:00 -0700

    install React and eslint; remove boilerplate code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # demo-04-client-goblins
+
+## Setup
+
+1. npx create-react-app .
+1. npm install eslint --save-dev
+1. npx eslint --init
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <h1>Hello...goblins are coming...</h1>
+  );
+};
+
+export default App;
