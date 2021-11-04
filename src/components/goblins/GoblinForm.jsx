8b02ac51f6f3a79959c50ef93a95592e5816174d
Franco Ortega <francopdx@gmail.com>
2021-11-04 14:48:17 -0700

    add hit points and armor class inputs

--- src/components/goblins/GoblinForm.jsx
+++ src/components/goblins/GoblinForm.jsx
@@ -4,6 +4,8 @@ import { insertGoblin } from '../../services/insertGoblin';
 
 const GoblinForm = ({ setGoblins }) => {
   const [name, setName] = useState('');
+  const [hitPoints, setHitPoints] = useState(5);
+  const [armorClass, setArmorClass] = useState(10);
 
   const onNameChange = ({ target }) => {
     setName(target.value);
@@ -22,25 +24,56 @@ const GoblinForm = ({ setGoblins }) => {
       .then(res => {
         setGoblins(prevState => [...prevState, res]);
       });
+  };
 
+  const onAddHitPoints = () => {
+    console.log('plus clicked');
+    setHitPoints(prevState => prevState + 1);
+  };
+
+  const onMinusHitPoints = () => {
+    console.log('minus clicked');
+    setHitPoints(prevState => prevState - 1);
+  };
+
+  const onIncreaseArmorClass = () => {
+    console.log('increase clicked');
+    setArmorClass(prevState => prevState + 1);
+  };
+
+  const onDecreaseArmorClass = () => {
+    console.log('decrease clicked');
+    setArmorClass(prevState => prevState - 1);
   };
 
   return (
     <div>
       <form onSubmit={onFormSubmit}>
-        <label htmlFor="goblin">Name:
-          <input id="goblin" type="text" onChange={onNameChange} />
+        <label htmlFor="name">Name:
+          <input id="name" type="text" onChange={onNameChange} />
         </label>
-        <label htmlFor="goblin">HP:
-          <input id="goblin" type="text" />
+        <label htmlFor="hit points">Hit Points:
+          <p
+            id="hit points"
+            type="text"
+            value={hitPoints}
+          >{hitPoints} </p>
+          <button type="button" onClick={onAddHitPoints}>+</button>
+          <button type="button" onClick={onMinusHitPoints}>-</button>
         </label>
-        <label htmlFor="goblin">AC:
-          <input id="goblin" type="text" />
+        <label htmlFor="armor class">Armor Class:
+          <p
+            id="armor class"
+            type="text"
+            value={armorClass}
+          >{armorClass} </p>
+          <button type="button" onClick={onIncreaseArmorClass}>+</button>
+          <button type="button" onClick={onDecreaseArmorClass}>-</button>
         </label>
         {/* <label htmlFor="goblin">Items:
           <input id="goblin" type="text" />
         </label> */}
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
       
     </div>
