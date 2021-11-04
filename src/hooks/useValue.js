b9bd3efed3a21dbc978e3aaa3e4d6cc25cf2f4c7
Franco Ortega <francopdx@gmail.com>
2021-11-04 16:03:12 -0700

    refactor useValue

--- src/hooks/useValue.js
+++ src/hooks/useValue.js
@@ -1,16 +1,16 @@
 import { useState } from 'react';
 
-export const useValue = (startingValue, valueSpender) => {
+export const useValue = (startingValue, setRemainingAvailable) => {
   const [value, setValue] = useState(startingValue);
 
   const increaseValue = () => {
     setValue(prevState => prevState + 1);
-    valueSpender(prevState => prevState - 1);
+    setRemainingAvailable(prevState => prevState - 1);
   };
 
   const decreaseValue = () => {
     setValue(prevState => prevState - 1);
-    valueSpender(prevState => prevState + 1);
+    setRemainingAvailable(prevState => prevState + 1);
   };
 
   return {
