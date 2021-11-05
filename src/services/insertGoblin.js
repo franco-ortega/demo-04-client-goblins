f97a85ae989e235662e2a5ea6518df97e4dab420
Franco Ortega <francopdx@gmail.com>
2021-11-05 16:47:29 -0700

    change services to use live server

--- src/services/insertGoblin.js
+++ src/services/insertGoblin.js
@@ -1,8 +1,8 @@
-// const API_URL = process.env.REACT_APP_API_URL;
-const LOCALHOST_URL = process.env.REACT_APP_LOCALHOST_URL;
+const API_URL = process.env.REACT_APP_API_URL;
+// const LOCALHOST_URL = process.env.REACT_APP_LOCALHOST_URL;
 
 export const insertGoblin = async(goblin) => {
-  return await fetch(LOCALHOST_URL, {
+  return await fetch(API_URL, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
